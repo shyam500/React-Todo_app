8a81f88e23a144c969d7f85510893e0f71e866be
shyam500 <shyamppk2000@gmail.com>
2022-02-26 21:36:42 +0530

    added todo storing functionality in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,37 @@
-import { useState } from 'react';
-import './App.css';
-import AddTodo from './components/AddTodo';
-import TodoList from './components/TodoList';
+import { useEffect, useState } from "react";
+import "./App.css";
+import AddTodo from "./components/AddTodo";
+import TodoList from "./components/TodoList";
 
 function App() {
-  const[todoList,setTodoList] = useState([]);
+  const [todoList, setTodoList] = useState([]);
 
-  const addTodoFunc=(todo)=>{
-    setTodoList([...todoList,todo]);
+  useEffect(() => {
+    if (localStorage.getItem("todoArr")) {
+      setTodoList(JSON.parse(localStorage.getItem("todoArr")));
+    }
+  }, []);
+
+  const addTodoFunc = (todo) => {
+    setTodoList([...todoList, todo]);
+    console.log(todoList);
+    localStorage.setItem("todoArr", JSON.stringify([...todoList]));
+  };
+
+  const deleteTodoHandler = (delTodo) => {
+    setTodoList((prev) => {
+      localStorage.setItem("todoArr", JSON.stringify([...prev.filter((a) => a !== delTodo)]));
+      prev.filter((a) => a !== delTodo);
+    });
+    
   };
 
   return (
     <div className="App">
-      <TodoList list={todoList}/>
-      <AddTodo addTodo={addTodoFunc}/>
+      <TodoList list={todoList} delTodoFunc={deleteTodoHandler} />
+      <AddTodo addTodo={addTodoFunc} />
     </div>
-  )
-};
+  );
+}
 
 export default App;
--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,18 +1,15 @@
-import { useRef } from "react";
 import "./TodoList.css";
 
 const TodoList = (props) => {
-  const listRef = useRef();
-  const { list } = props;
+  const { list, delTodoFunc } = props;
 
   const deleteHandler = (e) => {
-    //    const delItem =  e.target.parentElement.textContent.slice(0,-1);
-    const delItem = listRef.current
-     console.log(delItem);
+    const delItem = e.target.parentElement.textContent.slice(0, -2);
+    delTodoFunc(delItem);
   };
 
   const todos = list.map((todo) => (
-    <li key={todo} ref={listRef}>
+    <li key={todo}>
       {todo} <span onClick={deleteHandler}>X</span>
     </li>
   ));
